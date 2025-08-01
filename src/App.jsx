33229a8b237c7dfe299df9b890cee7528505bc4b
Aditi Yadav <aditiyadav2405@gmail.com>
2025-08-01 23:18:11 +0530

    done with Routing set-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Project';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 const App = () => {
   return (
-    <div>
-      <h1> Welcome to my Portfolio </h1>
-    </div>
-  )
-}
+    <>
+      <Navbar />
 
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h2>Hi, I'm Aditi Yadav </h2>
+      <p>Welcome to my portfolio website</p>
+    </div>
+  );
+};
+
+export default Home;
